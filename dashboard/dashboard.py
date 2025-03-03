418096a522c3e876cce2767ddd195ad5504ead0f
lailadwikartikasari <akunc4086@gmail.com>
2025-03-03 23:25:15 +0700

    update1

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,7 +9,7 @@ sns.set(style="whitegrid", context="talk")
 
 # Tentukan path absolut ke file CSV
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(BASE_DIR, "Bike_Sharing.csv")
+data_path = os.path.join(BASE_DIR, "merged_data.csv.csv")
 
 def load_data():
     """Load dataset dengan pengecekan error."""
